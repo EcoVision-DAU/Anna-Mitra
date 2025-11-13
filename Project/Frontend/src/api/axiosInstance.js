65fd9b3863f32c40b327830a7dfa27f2b8cd4da1
Hardip muliyasiya <hardikmuliyashiya5058@gmail.com>
2025-11-13 22:31:57 +0530

    Created static pages routes

--- Project/Frontend/src/api/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true, // if you're using cookies / sessions
-});
-
-export default api;
