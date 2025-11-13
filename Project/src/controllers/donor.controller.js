524398d262836dea4e5eec02c7f137f14a31434a
Hardip muliyasiya <hardikmuliyashiya5058@gmail.com>
2025-11-14 03:05:41 +0530

    Add Donor controller for donor profile and donation actions

---
+++ Project/src/controllers/donor.controller.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const DonorProfile = require('../models/donorprofile.model');
+const { donorProfileSchema } = require('../utils/joiSchemas');
+
+const catchAsync = require('../utils/catchAsync');
+
+// Create Donor Profile
+module.exports.createDonorProfile = async (userId, donorProfile) => {
+    donorProfile.userId = userId;
+    const newDonorProfile = new DonorProfile(donorProfile);
+    const createdDonorProfile = await newDonorProfile.save();
+    return createdDonorProfile; 
+}
+
+module.exports.viewDonorDashboard = catchAsync(async (req, res) => {
+    res.send("Welcome donor!");
+});
